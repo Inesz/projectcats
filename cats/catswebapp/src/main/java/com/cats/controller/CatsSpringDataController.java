47af7b5bc228e7a7bb7e3f41d8313b9bf130f4c3
Inesz <agnieszka-garsta@wp.pl>
2019-09-18 16:37:08 +0200

    log to file - SLF4J on top of the implementation (SLF4J, log to file by log4j2, loggerFactory and logManager, log properties: {appender.rolling, TriggeringPolicy, patterns})

--- cats/catswebapp/src/main/java/com/cats/controller/CatsSpringDataController.java
+++ cats/catswebapp/src/main/java/com/cats/controller/CatsSpringDataController.java
@@ -4,6 +4,8 @@ import com.cats.Cat;
 import com.cats.DTO.CatDTO;
 import com.cats.DAO.SpringDataDAO;
 import com.cats.service.CatsCRUDService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,11 +24,22 @@ import javax.validation.Valid;
 //@EnableJpaRepositories(transactionManagerRef="entityManagerFactory")
 @RequestMapping("/")
 public class CatsSpringDataController {
+    private static Logger LOGGER = LoggerFactory.getLogger(CatsSpringDataController.class);
+    private static org.apache.logging.log4j.Logger Log = org.apache.logging.log4j.LogManager.getLogger(CatsSpringDataController.class);
+
     @Autowired
     private CatsCRUDService catsCRUDService;
 
     @RequestMapping("")
     public String indexView(Model model) {
+        LOGGER.info("Java Logging with SLF4J info test");
+        LOGGER.debug("Java Logging with SLF4J debug test");
+        Cat cat = new Cat("TestCat");
+        LOGGER.error("Java Logging with SLF4J error test", cat);
+        Log.info("Java Logging with log4j info test");
+        Log.debug("Java Logging with log4j debug test");
+        Log.error("Java Logging with log4j error test", cat);
+        
         return "index";
     }
 
