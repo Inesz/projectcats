92657a0ef85617cd8b021dea947b5f49ba6a3859
Inesz <agnieszka-garsta@wp.pl>
2019-11-15 22:15:20 +0100

    SOAP getCatList from DB; servletContext rearrange (Servlet Config, Servlet Context)

--- cats/catswebapp/src/main/java/com/cats/serviceSOAP/CatsSOAPService.java
+++ cats/catswebapp/src/main/java/com/cats/serviceSOAP/CatsSOAPService.java
@@ -2,14 +2,18 @@ package com.cats.serviceSOAP;
 
 import com.cats.Cat;
 import com.cats.CatFoto;
+import com.cats.service.CatsCRUDService;
+import org.hibernate.Hibernate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import javax.annotation.Resource;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 
+@Transactional
 //endpointInterface need to be correct becouse of WebServiceException: Could not load Webservice SEI
 @WebService(
         endpointInterface = "com.cats.serviceSOAP.CatsSoapOperations",
@@ -19,20 +23,26 @@ import java.util.List;
 class CatsSOAPService implements CatsSoapOperations {
     private static Logger LOGGER = LoggerFactory.getLogger(CatsSOAPService.class);
 
+    @Resource
+    private CatsCRUDService catsCRUDService;
+
     /**
      * imitates downloading data from the database for reason of problems with autowiring DAO bean
      * no helped:
      * - annotated as @Service, @Controller, @Transactional..
      * - extends SpringBeanAutowiringSupport
      * - @PostConstruct
-     *     public void init() {
-     *         SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
-     *     }
+     * public void init() {
+     * SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
+     * }
      * init internal method field with new working, but still can`t get data from springCatDAO
      * attempts to solve the problem with dependencies and servlet configuration did not help
+     *
      * @return
+     *
+     * edit: changes in servlet context helped
      */
-    private List<Cat> getCatList() {
+    private List<Cat> getTestCatList() {
         LOGGER.info("Hibernate Error");
         List<Cat> catListLocal = new ArrayList<>();
 
@@ -46,6 +56,19 @@ class CatsSOAPService implements CatsSoapOperations {
         return catListLocal;
     }
 
+    private List<Cat> getCatList() {
+        List<Cat> catListLocal = null;
+
+        try {
+            catListLocal = catsCRUDService.selectCats();
+            catListLocal.forEach(c -> Hibernate.initialize(c.getCatFoto()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return catListLocal;
+    }
+
     @WebMethod()
     public String helloSOAP() {
         LOGGER.info("helloSOAP");
@@ -54,8 +77,7 @@ class CatsSOAPService implements CatsSoapOperations {
 
     @WebMethod()
     public List<Cat> catsList() {
-        LOGGER.info("CatList");
-        List<Cat> lc = getCatList();
-        return lc;
+        LOGGER.info("catsList");
+        return getCatList();
     }
 }
